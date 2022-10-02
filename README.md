36b506a322e0c675244779651477644cc9f9657b
KapDecy <z.bykov.d@gmail.com>
2022-10-02 20:34:38 +0300

    if, loops, if let, while let

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # mtuci-rs-course
 
-Matreials for lecture-
+Matreials for lecture-3
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let argv: Vec<String> = env::args().collect();
+    for arg in argv {
+        println!("{arg}");
+    }
+
+    if true {
+        println!("1")
+    } else {
+        println!("2")
+    }
+
+    while false {
+        println!("3")
+    }
+
+    for _ in 1..=1 {
+        println!("4")
+    }
+
+    loop {
+        println!("5");
+        if true {
+            break;
+        }
+    }
+
+    let mut o = Some(1);
+    match o {
+        // а - пример деструктуризации
+        Some(a) => {
+            println!("a={a}")
+        }
+        None => {
+            println!("None")
+        }
+    }
+
+    if let Some(a) = o {
+        println!("{a}");
+    }
+
+    while let Some(_a) = o {
+        o = None;
+    }
 }
